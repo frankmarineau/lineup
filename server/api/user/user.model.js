c7078f5ab8aeacc1ac8c73014dc2228a99c0eee4
William Turner <willtur.will@gmail.com>
2014-11-08 12:16:06 -0500

    API Config

--- server/api/user/user.model.js
+++ server/api/user/user.model.js
@@ -8,9 +8,10 @@ var authTypes = ['github', 'twitter', 'facebook', 'google'];
 var UserSchema = new Schema({
   name: String,
   email: { type: String, lowercase: true },
+  phone: { type: String },
   role: {
     type: String,
-    default: 'user'
+    default: 'guest'
   },
   hashedPassword: String,
   provider: String,
