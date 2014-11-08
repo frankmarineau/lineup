bf8faa0d48314814fd418e0631716f50ebc211dc
babin93 <mababin93@gmail.com>
2014-11-08 15:35:34 -0500

    Added Guests to Checkin

--- client/app/checkin/checkin.controller.js
+++ client/app/checkin/checkin.controller.js
@@ -2,13 +2,30 @@
 
 angular.module('lineupApp')
   .controller('CheckinCtrl', function ($scope) {
-    $scope.message = 'Hello';
+    $scope.message = 'Hello';    
 
-    $scope.LineupName = function(){
+    $scope.GetLineupName = function(){
 
     	// ajax call ??
-    	var lineupName = "Emergency"
-    	return lineupName;
-
+    	return 'Emergency'
     };
+
+    $scope.lineupName = $scope.GetLineupName();
+
+    var Guest = function( firstName, lastName, numberOfAttendees, phoneNumer){
+    	return {
+    		firstName: firstName,
+    		lastName: lastName,
+    		numberOfAttendees: numberOfAttendees,
+    		phoneNumer: phoneNumer,
+    		fullName : function(){
+    			return firstName + ' ' + lastName;
+    		}
+    	};
+    };
+
+    $scope.guestList = [
+    	Guest('Marc-Antoine', 'Babin', 4, '819-238-1313'),
+		Guest('Olivier', 'Charbonneau', 2, '819-238-1313'),
+    	];
   });
