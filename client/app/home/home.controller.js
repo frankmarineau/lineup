1900d54bdd01d7c32e9b1c46e27d0c7f556b26bd
Francis Marineau <frank.marineau@gmail.com>
2014-11-09 12:27:12 -0500

    Fixed SMS link

--- client/app/home/home.controller.js
+++ client/app/home/home.controller.js
@@ -4,12 +4,10 @@ angular.module('lineupApp')
   .controller('HomeCtrl', function ($scope, Auth, $location, User) {
     if (Auth.isLoggedIn()) {
       Auth.getCurrentUser().$promise.then(function(user) {
-        console.log(user.role);
         if (user.role === "admin" || user.role === "clerk") {
           $location.path('/lineups');
         }
         else if (user.role === "user") {
-          console.log("MEOW");
           $location.path('/guests');
         }
       });
