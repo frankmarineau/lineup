93c2ac95b8df3de091e838cb14555ed41162114a
Francis Marineau <frank.marineau@gmail.com>
2014-11-09 06:50:05 -0500

    basic layout for guest login

--- client/app/clientLogin/clientLogin.controller.js
+++ client/app/clientLogin/clientLogin.controller.js
@@ -3,7 +3,6 @@
 angular.module('lineupApp')
   .controller('ClientloginCtrl', function ($scope, $routeParams, $window) {
     $scope.loginOauth = function(provider) {
-      console.log('/auth/' + provider + '?state=' + $routeParams.lineupUserId);
       $window.location.href = '/auth/' + provider + '?state=' + $routeParams.lineupUserId;
     };
   });
