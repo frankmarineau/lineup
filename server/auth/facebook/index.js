1875072e43b505ce007b66cd86d3a49ce3f6bbbd
William Turner <willtur.will@gmail.com>
2014-11-09 04:57:03 -0500

    Attempt at facebook login for guests

--- server/auth/facebook/index.js
+++ server/auth/facebook/index.js
@@ -3,19 +3,37 @@
 var express = require('express');
 var passport = require('passport');
 var auth = require('../auth.service');
+var Lineupuser = require('../../api/lineupuser/lineupuser.model');
 
 var router = express.Router();
 
 router
-  .get('/', passport.authenticate('facebook', {
-    scope: ['email', 'user_about_me'],
-    failureRedirect: '/signup',
-    session: false
-  }))
+  .get('/', function (req, res, next) {
+    passport.authenticate('facebook', {
+      scope: ['email', 'user_about_me'],
+      failureRedirect: '/signup',
+      session: false,
+      state: req.query.state
+    })(req, res, next);
+  })
 
   .get('/callback', passport.authenticate('facebook', {
     failureRedirect: '/signup',
     session: false
-  }), auth.setTokenCookie);
+  }), auth.setTokenCookie, function (req, res, next) {
+    if (req.query.state && req.user && req.user._id) {
+      Lineupuser.findById(req.query.state, function (err, lineupuser) {
+        if (err) return next(err);
+        if (lineupuser) {
+          lineupuser.user = req.user._id;
+          lineupuser.save(function (err, lineupuser) {
+            return next(err);
+          });
+        }
+      });
+    } else {
+      return next();
+    }
+  });
 
 module.exports = router;
