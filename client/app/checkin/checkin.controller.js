0eccce02133128b051736417a3d639d59a14348f
Francis Marineau <frank.marineau@gmail.com>
2014-11-09 03:37:09 -0500

    Implement checkout

--- client/app/checkin/checkin.controller.js
+++ client/app/checkin/checkin.controller.js
@@ -1,14 +1,8 @@
 'use strict';
 
 angular.module('lineupApp')
-  .controller('CheckinCtrl', function ($scope, Checkin, Lineup, $routeParams) {
+  .controller('CheckinCtrl', function ($scope, Lineup, $routeParams) {
     $scope.lineup = Lineup.get({id: $routeParams.id});
-
-    var refreshGuestList = function() {
-    	$scope.checkin = Checkin.query();
-    }
-
-    refreshGuestList();
 
     $scope.addGuest = function() {
         Lineup.update({
@@ -17,26 +11,26 @@ angular.module('lineupApp')
             name: $scope.user.name,
             phone: $scope.user.phone
         }, function(newGuest) {
-            $scope.checkin.guests.push(newGuest);
+            $scope.lineup.users.push(newGuest);
         });
     };
 
     $scope.checkout = function(index) {
         Lineup.checkout({
-            id: $scope.lineup._id
+            id: $scope.lineup.users[index]._id
         }, function(newGuest) {
-            $scope.checkin.guests.splice(index, 1);
+            $scope.lineup.users.splice(index, 1);
         });
     };
 
     $scope.deleteGuest = function(index){
         Lineup.noshow({
-            id: $scope.lineup._id
+            id: $scope.lineup.users[index]._id
         }, function(newGuest) {
-            $scope.checkin.guests.splice(index, 1);
+            $scope.lineup.users.splice(index, 1);
         });
 
-    	$scope.checkin.guests.splice(index, 1);
+    	$scope.lineup.users.splice(index, 1);
     };
 
   });
