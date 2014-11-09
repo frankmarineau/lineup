16ca207a8ecd97cadf8e4680ebd7329a0edbe268
Francis Marineau <frank.marineau@gmail.com>
2014-11-08 23:41:44 -0500

    login redirection caline de bine stais long sorry

--- client/app/clientLineupDetails/clientLineupDetails.js
+++ client/app/clientLineupDetails/clientLineupDetails.js
@@ -3,7 +3,7 @@
 angular.module('lineupApp')
   .config(function ($routeProvider) {
     $routeProvider
-      .when('/guests/:id', {
+      .when('/guests', {
         templateUrl: 'app/clientLineupDetails/clientLineupDetails.html',
         controller: 'ClientlineupdetailsCtrl',
         authenticate: true
--- client/app/home/home.controller.js
+++ client/app/home/home.controller.js
@@ -1,10 +1,20 @@
 'use strict';
 
 angular.module('lineupApp')
-  .controller('HomeCtrl', function ($scope) {
+  .controller('HomeCtrl', function ($scope, Auth, $location, User) {
+    if (Auth.isLoggedIn()) {
+      var user = Auth.getCurrentUser();
+      if (user.role === "admin" || user.role === "clerk") {
+        $location.path('/lineups');
+      }
+      else if (user.role === "user") {
+        $location.path('/guests');
+      }
+    }
+
     $('#next').on('click', function() {
         $('html, body').animate({
-            scrollTop: $('#what').offset().top -60
+          scrollTop: $('#what').offset().top - 60
         }, 1000);
     });
   });
