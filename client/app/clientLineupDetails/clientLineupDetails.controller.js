b6c40eee9ac6b03d0f6ee8dc94a8f25857b4878f
Francis Marineau <frank.marineau@gmail.com>
2014-11-09 11:39:41 -0500

    asd

--- client/app/clientLineupDetails/clientLineupDetails.controller.js
+++ client/app/clientLineupDetails/clientLineupDetails.controller.js
@@ -1,17 +1,16 @@
 'use strict';
 
 angular.module('lineupApp')
-  .controller('ClientlineupdetailsCtrl', function ($scope, Auth, ClientLineup) {
-    var lineups = ClientLineup.query();
-    $scope.isInQueue = lineups.length > 0;
+  .controller('ClientlineupdetailsCtrl', function ($scope, Auth, Lineup) {
+    Lineup.query({}, function(lineups) {
+        $scope.lineup = lineups[0] || [];
+        $scope.isInQueue = lineups.length > 0;
+    });
 
-    var name = Auth.getCurrentUser().name;
+    var user = Auth.getCurrentUser();
+    var name = user.name;
     var words = name.split(' ');
     var firstName = words.length > 0 ? words[0] : null;
-    
-    $scope.firstName = firstName;
 
-    if (lineups.length > 0) {
-        $scope.lineup = lineups[0];
-    }
+    $scope.firstName = firstName;
   });
