1ce9bcae498b81082af219a52f6689abc47bc588
William Turner <willtur.will@gmail.com>
2014-11-08 19:53:55 -0500

    Lineup controller stubs + admin create

--- server/api/lineup/lineup.controller.js
+++ server/api/lineup/lineup.controller.js
@@ -4,39 +4,72 @@ var _ = require('lodash');
 var Lineup = require('./lineup.model');
 var Lineupuser = require('../lineupuser/lineupuser.model');
 
-exports.index = function(req, res) {
+exports.index = function (req, res) {
   if (req.user.hasRole('admin')) {
     Lineup.find({ owner: req.user._id }, function (err, lineups) {
       if (err) return handleError(res, err);
       return res.json(200, lineups);
     });
   } else if (req.user.hasRole('clerk')) {
-    return res.send(501, 'Not Implemented');
+    return res.json(501, 'Not Implemented');
   } else {
-    return res.send(501, 'Not Implemented');
+    return res.json(501, 'Not Implemented');
   }
 };
 
-exports.show = function(req, res) {
+exports.show = function (req, res) {
   if (req.user.hasRole('clerk')) {
     Lineup.findById(req.params.id, function (err, lineup) {
       if (err) return handleError(res, err);
-      if (!lineup) return res.send(404);
+      if (!lineup) return res.json(404);
       Lineupuser.find({ lineup: lineup._id }).populate('user').exec(function (err, lineupusers) {
         if (err) return handleError(res, err);
         lineup.clients = lineupusers;
-        return res.json(lineup);
+        return res.json(200, lineup);
       });
     });
   } else {
     Lineup.findById(req.params.id, function (err, lineup) {
       if (err) return handleError(res, err);
-      if (!lineup) return res.send(404);
-      return res.json(lineup);
+      if (!lineup) return res.json(404);
+      return res.json(200, lineup);
     });
   }
 };
 
+exports.create = function (req, res) {
+  if (req.user.hasRole('admin')) {
+    Lineup.create(req.body, function (err, lineup) {
+      if (err) return handleError(res, err);
+      return res.json(200, lineup);
+    });
+  } else if (req.user.hasRole('clerk')) {
+    return res.json(501, 'Not Implemented');
+  } else {
+    return res.json(501, 'Not Implemented');
+  }
+};
+
+exports.update = function (req, res) {
+  if (req.user.hasRole('admin')) {
+    return res.json(501, 'Not Implemented');
+  } else if (req.user.hasRole('clerk')) {
+    return res.json(501, 'Not Implemented');
+  } else {
+    return res.json(501, 'Not Implemented');
+  }
+};
+
+exports.destroy = function (req, res) {
+  if (req.user.hasRole('admin')) {
+    return res.json(501, 'Not Implemented');
+  } else if (req.user.hasRole('clerk')) {
+    return res.json(501, 'Not Implemented');
+  } else {
+    return res.json(501, 'Not Implemented');
+  }
+};
+
 function handleError(res, err) {
-  return res.send(500, err);
+  return res.json(500, err);
 }
